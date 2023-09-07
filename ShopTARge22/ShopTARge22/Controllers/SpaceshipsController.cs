0a527404d8ead5e5f1794f5b87d6baba38ea6566
Ingvar Derešivski <inde999@gmail.com>
2023-09-07 12:13:44 +0300

    SpaceshipIndex

---
+++ ShopTARge22/ShopTARge22/Controllers/SpaceshipsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge22.Data;
+using ShopTARge22.Models.Spaceships;
+
+namespace ShopTARge22.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly ShopTARge22Context _context;
+
+        public SpaceshipsController
+            (
+                ShopTARge22Context context
+            )
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipsIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    BuiltDate = x.BuiltDate,
+                    Passengers = x.Passengers
+                });
+
+            return View();
+        }
+    }
+}
--- ShopTARge22/ShopTARge22/Program.cs
+++ ShopTARge22/ShopTARge22/Program.cs
@@ -1,7 +1,14 @@
+using ShopTARge22.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//builder.Services.AddDbContext<ShopTARge22Context>(options =>
+//    options.UseSqlServer(()));
+
 
 var app = builder.Build();
 
@@ -22,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Error}/{id?}");
 
 app.Run();
