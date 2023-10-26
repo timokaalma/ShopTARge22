e6c2e0aebf16a922e49506e48a816c8c70ee452f
Ingvar Derešivski <inde999@gmail.com>
2023-10-26 16:56:11 +0300

    WeatherAPI

--- ShopTARge22/ShopTARge22.ApplicationServices/Services/WeatherForecastServices.cs
+++ ShopTARge22/ShopTARge22.ApplicationServices/Services/WeatherForecastServices.cs
@@ -10,7 +10,7 @@ namespace ShopTARge22.ApplicationServices.Services
 
         public async Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto)
         {
-            string idOpenWeather = "e902b03c8b29e77aae2b8171279d8352";
+            string idOpenWeather = "yourAPIKey";
             string url = $"https://api.openweathermap.org/data/2.5/weather?q={dto.City}&units=metric&appid={idOpenWeather}";
 
             using (WebClient client = new WebClient())
