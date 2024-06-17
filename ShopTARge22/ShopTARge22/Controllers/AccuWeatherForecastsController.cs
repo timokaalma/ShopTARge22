5a010be1ab937c61de75258629fa08667ed87452
timok <timokaalma@gmail.com>
2024-06-17 23:46:52 +0300

    AccuWeather controller, interfaces, vms

---
+++ ShopTARge22/ShopTARge22/Controllers/AccuWeatherForecastsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARge22.Core.ServiceInterface;
+
+namespace ShopTARge22.Controllers
+{
+    public class AccuWeatherForecastsController : Controller
+    {
+        private readonly IAccuWeatherForecastsServices _accuWeatherForecastsServices;
+
+        public AccuWeatherForecastsController
+            (IAccuWeatherForecastsServices accuWeatherForecastsServices)
+        {
+            _accuWeatherForecastsServices = accuWeatherForecastsServices;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchCity(AccuWeatherSearchCityViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("City", "AccuWeatherForecasts", new { city = model.City });
+            }
+        }
+
+    }
+}
